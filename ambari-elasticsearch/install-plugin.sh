9c3a6071d7387055da0fb7434bf9d2f4cf083eb4
zhangshuyu <zhangshuyu@haima.me>
2018-07-09 16:06:17 +0800

    修改 安装脚本

--- ambari-elasticsearch/install-plugin.sh
+++ ambari-elasticsearch/install-plugin.sh
@@ -2,19 +2,23 @@
 
 install(){
     # stacks
-    rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH
-    mkdir /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH
+    if [ ! -d "/var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH" ];then
+    echo "创建 /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH"
+    mkdir -p /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH
+    fi
     cp -R stacks/* /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH/
 
     # common-services
-    rm -rf /var/lib/ambari-server/resources/common-services/ELASTICSEARCH
+    if [ ! -d "/var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0" ];then
+    echo "创建 /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0"
     mkdir -p /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0
+    fi
     cp -R common-services/* /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0
 }
 
 checkUser(){
     if [ `whoami` != "root" ];then
-        echo " only root can run me"
+        echo "only root can run me"
         exit 1
     else
         install
