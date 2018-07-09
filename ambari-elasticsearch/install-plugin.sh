20db4f2841088abc86ac1aeddf8b2ccc900122ed
zhangshuyu <zhangshuyu@haima.me>
2018-07-09 15:39:55 +0800

    区分Master Node和Data Node

---
+++ ambari-elasticsearch/install-plugin.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+install(){
+    # stacks
+    rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH
+    mkdir /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH
+    cp -R stacks/* /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH/
+
+    # common-services
+    rm -rf /var/lib/ambari-server/resources/common-services/ELASTICSEARCH
+    mkdir -p /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0
+    cp -R common-services/* /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0
+}
+
+checkUser(){
+    if [ `whoami` != "root" ];then
+        echo " only root can run me"
+        exit 1
+    else
+        install
+    fi
+}
