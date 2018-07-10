4f0ef23ef4b35735ac728a25d28c4efa72f54d00
zhangshuyu <zhangshuyu@haima.me>
2018-07-10 18:16:19 +0800

    修改结构

--- elasticsearch/install-plugin.sh
+++ elasticsearch/install-plugin.sh
@@ -7,16 +7,13 @@ install(){
     mkdir -p /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH
     fi
     cp -R stacks/* /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH/
-    cp -R common-services/configuration /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH/
-    cp -R common-services/package /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH/
-    cp -R common-services/quicklinks /var/lib/ambari-server/resources/stacks/HDP/2.6/services/ELASTICSEARCH/
 
     # common-services
-    if [ ! -d "/var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0" ];then
-    echo "创建 /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0"
-    mkdir -p /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0
-    fi
-    cp -R common-services/* /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0
+    #if [ ! -d "/var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0" ];then
+    #echo "创建 /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0"
+    ##mkdir -p /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0
+    #fi
+    #cp -R common-services/* /var/lib/ambari-server/resources/common-services/ELASTICSEARCH/6.3.0
 }
 
 checkUser(){
